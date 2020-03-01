a9bb2604c9ba4a31b3e9f31fcad7bd4c06d6ef0d
DanAnastasyev <dan.anastasyev@gmail.com>
2020-03-01 17:03:41 +0300

    Fixed download model script

--- download_model.sh
+++ download_model.sh
@@ -5,7 +5,11 @@ download_from_google_drive() {
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" -O $2 && rm -rf /tmp/cookies.txt
 }
 
+if [[ ! -d models ]]
+then
+    mkdir models
+fi
+
 download_from_google_drive "1XiCj0OXZtBfxKyhMEPodt6wp2Jpc3RI9" "model.tar" \
-    && mkdir models \
     && tar -xvf model.tar -C models \
     && rm model.tar
